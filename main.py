0b64eb877ba579920a504c7c9e1ec2b4c24b2518
nuyhank <nuyhank@gmail.com>
2024-04-11 09:59:40 +0000

    init: main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for idx, val in enumerate(nums):
+        if target - val in nums[idx + 1:]:
+            return [idx, nums[idx + 1:].index(target - val) + (idx + 1)]
+    return []
